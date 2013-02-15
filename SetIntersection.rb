2e4ce944b7699d8e2838324af6dcfa0754444615
Joe Letizia <joe.letizia@gmail.com>
2013-02-15 17:07:27 -0500

    Commented out carpet bombs...

--- SetIntersection.rb
+++ SetIntersection.rb
@@ -8,23 +8,23 @@ File.open(ARGV[0]).each_line do |line|
 	puts list1.join ","
 	puts list2.join ","
 	until list1.length == 0
-		puts "ITERATION"
+		#puts "ITERATION"
 		if list1.first.to_i == list2.first.to_i
-			puts list1.first + " is equal to than " + list2.first
-			puts "load " + list2.first
+			#puts list1.first + " is equal to than " + list2.first
+			#puts "load " + list2.first
 			intersections << list1.first
-			puts "delete " + list1.first
+			#puts "delete " + list1.first
 			list1.delete list1.first
-			puts "delete " + list2.first
+			#puts "delete " + list2.first
 			list2.delete list2.first
 		else
 			if list1.first.to_i > list2.first.to_i
-				puts list1.first + " is greater than " + list2.first
-				puts "delete " + list2.first
+				#puts list1.first + " is greater than " + list2.first
+				#puts "delete " + list2.first
 				list2.delete list2.first
 			else
-				puts list1.first + " is less than " + list2.first
-				puts "delete " + list1.first
+				#puts list1.first + " is less than " + list2.first
+				#puts "delete " + list1.first
 				list1.delete list1.first
 			end
 		end
