4cad5c7eb3b7dbf0262c028661bdbd6639aa22c2
Joe Letizia <joe.letizia@gmail.com>
2013-11-11 08:20:59 -0500

    finished double squares

--- double_squares.rb
+++ double_squares.rb
@@ -1,7 +1,7 @@
 module DoubleSquares
   def self.check n
     squares = []
-    (0..Math.sqrt(n).to_i).each do |x|              # loop until sqrt, because it cant be bigger
+    (0..Math.sqrt(n).floor).each do |x|              # loop until sqrt, because it cant be bigger
       y2 = n - x*x
       if perfect_square?(y2)                                   # if y is an int, its a square
         squares.push Math.sqrt(y2) unless squares.include?(Math.sqrt(y2))
@@ -11,6 +11,18 @@ module DoubleSquares
   end
 
   def self.perfect_square? k
-    Math.sqrt(k) == Math.sqrt(k).to_i
+    Math.sqrt(k) == Math.sqrt(k).to_i rescue puts "exception on #{k}"
   end
+end
+
+
+lines = []
+File.open(ARGV[0]).each_line do |line|
+  lines << line
+end
+
+lines.delete_at 0
+
+lines.each do |line|
+  puts DoubleSquares.check(line.to_i)
 end
