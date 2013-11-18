9d3981fbe7d071084f41b39e0c2cc7be2969e855
Joe Letizia <pair+joe@joeletizia.com>
2013-11-18 08:57:58 -0500

    reformatted test so they can handle new cases

--- spec/number_pairs_spec.rb
+++ spec/number_pairs_spec.rb
@@ -2,14 +2,17 @@ require 'spec_helper'
 require './number_pairs.rb'
 
 describe NumberPairsFinder do
-  let(:numbers) { [1,2,3,4,6] }
-  let(:target) { 5 }
 
   let(:finder) { NumberPairsFinder.new(numbers, target) }
 
   describe "#find_pairs" do
-    it "returns the proper values" do
-      finder.find_pairs.should == [[1,4],[2,3]]
+    context "with the following set of numbers" do
+      let(:numbers) { [1,2,3,4,6] }
+      let(:target) { 5 }
+
+      it "returns the proper pairs" do
+        finder.find_pairs.should == [[1,4],[2,3]]
+      end
     end
   end
 end
